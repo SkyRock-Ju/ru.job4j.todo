f81862f92ae1d0d8c975618622c206079c67687e
Dulustan <skyrockpk@gmail.com>
2023-05-05 22:54:43 +0700

    configured main class TodoApplication

--- src/main/java/ru/job4j/todo/TodoApplication.java
+++ src/main/java/ru/job4j/todo/TodoApplication.java
@@ -1,13 +1,25 @@
 package ru.job4j.todo;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Bean(destroyMethod = "close")
+	public SessionFactory sf() {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure().build();
+		return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+	}
 }
