cc41b1b498ee7d36dbe057ffac473ea3cf812a85
Dulustan <skyrockpk@gmail.com>
2023-05-05 22:54:20 +0700

    added TaskService

---
+++ src/main/java/ru/job4j/todo/service/TaskService.java
@@ -0,0 +1,31 @@
+package ru.job4j.todo.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.store.TaskStore;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TaskService {
+    private TaskStore taskStore;
+
+    public Optional<Task> findById(int id) {
+        return taskStore.findById(id);
+    }
+    public List<Task> findAll() {
+        return taskStore.findAll();
+    }
+    public Task save(Task task) {
+        return taskStore.save(task);
+    }
+    public boolean update(Task task) {
+        return taskStore.update(task);
+    }
+    public boolean delete(int id) {
+        return taskStore.delete(id);
+    }
+}
