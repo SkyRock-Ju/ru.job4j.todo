428906e8f1f72f9041f112711cc61bf6b210613f
Dulustan <skyrockpk@gmail.com>
2023-05-05 22:53:37 +0700

    added Task model

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,20 @@
+package ru.job4j.todo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private LocalDateTime created;
+    private boolean isDone;
+}
